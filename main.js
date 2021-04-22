c05456ebab5d53e5f8b460adb36582e81ba59c89
YurleyTorres <marly.torres.silva@gmail.com>
2021-04-21 20:51:56 -0500

    2021-04-09

---
+++ main.js
@@ -0,0 +1,10 @@
+let edad;
+
+edad = prompt("Ingrese la edad:");
+
+if(edad > 18){
+    console.log("Es mayor de edad");
+}
+else{
+    console.log("La edad ingresada no cumple con los parámetros");
+}
